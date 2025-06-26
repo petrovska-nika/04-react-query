e18f76816228e98163f74c1b72bda77486f37f39
petrovska-nika <petrovska.nika.dev@gmail.com>
2025-06-26 23:27:01 +0300

    fix-cafe

--- src/main.tsx
+++ src/main.tsx
@@ -1,2 +1,12 @@
 import "modern-normalize/modern-normalize.css";
 import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App/App.tsx";
+import "./index.css"; // або './style.css', залежно від того, який файл у тебе є
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
