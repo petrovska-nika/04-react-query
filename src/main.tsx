fe3a212a1c4fcbf00195aac27a55d57199241b16
petrovska-nika <petrovska.nika.dev@gmail.com>
2025-06-26 23:32:38 +0300

    cafe-fix

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App.tsx";
-import "./index.css"; // або './style.css', залежно від того, який файл у тебе є
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
