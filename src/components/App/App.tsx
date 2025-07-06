875fee96dc3ab56160f54eb339b75aba7ef333bc
petrovska-nika <petrovska.nika.dev@gmail.com>
2025-07-06 14:31:51 +0300

    fix-app

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react";
 import toast, { Toaster } from "react-hot-toast";
-import { Movie } from "./types/movie";
-import { fetchMovies } from "./services/movieService";
+import { Movie } from "../../types/movie";
+import { fetchMovies } from "../../services/movieService";
 
-import SearchBar from "./components/SearchBar/SearchBar";
-import MovieGrid from "./components/MovieGrid/MovieGrid";
-import Loader from "./components/Loader/Loader";
-import ErrorMessage from "./components/ErrorMessage/ErrorMessage";
-import MovieModal from "./components/MovieModal/MovieModal";
+import SearchBar from "./SearchBar/SearchBar";
+import MovieGrid from "./MovieGrid/MovieGrid";
+import Loader from "./Loader/Loader";
+import ErrorMessage from "./ErrorMessage/ErrorMessage";
+import MovieModal from "./MovieModal/MovieModal";
 
 export default function App() {
   const [query, setQuery] = useState("");
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import "modern-normalize/modern-normalize.css";
 import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./components/App/App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
